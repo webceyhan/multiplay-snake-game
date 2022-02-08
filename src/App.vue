bb906a91fa726577b4ab049ea32cd35e0b24e579
Evren Ceyhan <evren@ceyhan.io>
2022-02-08 16:12:27 +0300

    basic drawing methods impl. with game state

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,50 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-// import HelloWorld from './components/HelloWorld.vue'
-
 import { onMounted, ref } from 'vue';
+// import HelloWorld from './components/HelloWorld.vue'
 
 // get canvas ref
 const grid = ref();
+const GRID_SIZE = 600;
+const CELL_COUNT = 20;
+const CELL_SIZE = GRID_SIZE / CELL_COUNT;
 
+
+
+// define game state
+const state = {
+  player: {
+    color: '#00f',
+    position: { x: 2, y: 10 },
+    velocity: { x: 1, y: 0 },
+    snake: [
+      { x: 1, y: 10 },
+      { x: 2, y: 10 },
+    ]
+  },
+};
 
 onMounted(() => {
+  // define grid size (mandatory for canvas)
+  grid.value.width = grid.value.height = GRID_SIZE;
+
   // define canvas context in 2D
+  /** @var {CanvasRenderingContext2D} */
   const ctx = grid.value.getContext('2d');
 
+  const drawCell = ({ x, y }, color) => {
+    ctx.fillStyle = color;
+    ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+  }
+
+  const drawPlayer = ({ snake, color }) => {
+    snake.forEach((cell) => drawCell(cell, color));
+  }
+
+  const drawGame = (state) => {
+    drawPlayer(state.player);
+  }
+
+  drawGame(state);
 });
 
 </script>
@@ -21,7 +53,11 @@ onMounted(() => {
   <div class="container text-center">
     <h1 class="display-4 py-3">Snake Game</h1>
 
-    <canvas ref="grid" class="bg-secondary" style="width: 600px; height: 600px;"></canvas>
+    <canvas
+      ref="grid"
+      class="bg-secondary"
+      :style="{ width: `${GRID_SIZE}px`, height: `${GRID_SIZE}px` }"
+    ></canvas>
   </div>
 </template>
 
