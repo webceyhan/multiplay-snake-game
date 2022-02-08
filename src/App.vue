0843e1ea9b9f757b7fce407452ff51f4101e76f2
Evren Ceyhan <evren@ceyhan.io>
2022-02-08 14:39:52 +0300

    basic canvas created

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+import { onMounted, ref } from 'vue';
+
+// get canvas ref
+const grid = ref();
+
+
+onMounted(() => {
+  // define canvas context in 2D
+  const ctx = grid.value.getContext('2d');
+
+});
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="container text-center">
+    <h1 class="display-4 py-3">Snake Game</h1>
+
+    <canvas ref="grid" class="bg-secondary" style="width: 600px; height: 600px;"></canvas>
+  </div>
 </template>
 
