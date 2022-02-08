2c79e2966e72393e68b63fd41c55f7e368a7386f
Evren Ceyhan <evren@ceyhan.io>
2022-02-08 17:09:14 +0300

    game loop and movement added

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,30 @@
 import { onMounted, ref } from 'vue';
 // import HelloWorld from './components/HelloWorld.vue'
 
+// define Bootstrap theme colors
+const colors = {
+  primary: '#007bff',
+  secondary: '#6c757d',
+  success: '#28a745',
+  danger: '#dc3545',
+  warning: '#ffc107',
+  info: '#17a2b8',
+  light: '#f8f9fa',
+  dark: '#343a40',
+};
+
 // get canvas ref
 const grid = ref();
 const GRID_SIZE = 600;
 const CELL_COUNT = 20;
 const CELL_SIZE = GRID_SIZE / CELL_COUNT;
-
-
+const GRID_COLOR = colors.dark;
+const SNAKE_COLOR = colors.primary;
 
 // define game state
 const state = {
   player: {
-    color: '#00f',
+    color: SNAKE_COLOR,
     position: { x: 2, y: 10 },
     velocity: { x: 1, y: 0 },
     snake: [
@@ -31,9 +43,9 @@ onMounted(() => {
   /** @var {CanvasRenderingContext2D} */
   const ctx = grid.value.getContext('2d');
 
-  const drawCell = ({ x, y }, color) => {
+  const drawCell = ({ x, y, w, h }, color) => {
     ctx.fillStyle = color;
-    ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, w || CELL_SIZE, h || CELL_SIZE);
   }
 
   const drawPlayer = ({ snake, color }) => {
@@ -41,10 +53,31 @@ onMounted(() => {
   }
 
   const drawGame = (state) => {
+    // draw canvas to clear for next frame
+    drawCell({ x: 0, y: 0, w: GRID_SIZE, h: GRID_SIZE }, GRID_COLOR);
+
+    // draw player
     drawPlayer(state.player);
   }
 
-  drawGame(state);
+  const movePlayer = ({ position, velocity, snake }) => {
+    position.x += velocity.x;
+    position.y += velocity.y;
+
+    // add new cell to the end of the snake
+    snake.push({ ...position });
+    // remove old cell from the beginning of the snake
+    snake.shift();
+  }
+
+  const loopGame = (state) => {
+    movePlayer(state.player);
+    drawGame(state);
+  }
+
+  const gameLoop = setInterval(() => {
+    loopGame(state);
+  }, 1000);
 });
 
 </script>
@@ -55,7 +88,7 @@ onMounted(() => {
 
     <canvas
       ref="grid"
-      class="bg-secondary"
+      class="bg-dark"
       :style="{ width: `${GRID_SIZE}px`, height: `${GRID_SIZE}px` }"
     ></canvas>
   </div>
