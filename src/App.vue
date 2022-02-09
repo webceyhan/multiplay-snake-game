ee002f3a75d578f4878ca4ffd418b9b0557ca4a8
Evren Ceyhan <evren@ceyhan.io>
2022-02-09 23:26:06 +0300

    virtual keyboard imp. for mobile

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, ref, watch } from 'vue';
 import { createGameContext, GRID_SIZE, useGame } from './game';
+import VirtualKeyboard from './components/VirtualKeyboard.vue';
 
 // get canvas ref
 const canvas = ref();
@@ -8,8 +9,18 @@ const canvas = ref();
 // get state object
 const { state, active, message, emit } = useGame();
 
+const isMobile = () => {
+  const ua = navigator.userAgent.toLowerCase();
+  const iPad = ua.indexOf('ipad') > -1 || ua.indexOf('macintosh') > -1 && 'ontouchend' in document;
+  return iPad || /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(ua);
+}
+
 function onStart() {
   emit('start')
+}
+
+function onKeydown(key) {
+  emit('keydown', key);
 }
 
 onMounted(() => {
@@ -20,31 +31,37 @@ onMounted(() => {
   watch(state, () => gameCtx.draw(state.value));
 
   // watch for keydown events
-  document.addEventListener('keydown', ({ key }) => emit('keydown', key));
+  document.addEventListener('keydown', ({ key }) => onKeydown(key));
 });
 
 </script>
 
 <template>
-  <div class="container text-center">
-    <div class="d-flex justify-content-between align-items-center px-5 py-3">
+  <div class="container">
+    <header class="text-center mb-4">
       <h1 class="display-4">Snake Game</h1>
+    </header>
 
+    <section class="d-flex justify-content-center">
       <button
         v-if="!active"
         type="button"
-        class="btn btn-primary btn-lg"
+        class="btn btn-primary btn-lg mb-4"
         @click="onStart"
       >Start Game</button>
-    </div>
 
-    <canvas
-      ref="canvas"
-      class="bg-dark"
-      :style="{ width: `${GRID_SIZE}px`, height: `${GRID_SIZE}px` }"
-    ></canvas>
+      <virtual-keyboard v-if="active && isMobile()" class="mb-4" @press="onKeydown" />
+    </section>
 
-    <h1 v-if="message" class="display-6">{{ message }}</h1>
+    <section class="d-flex flex-column justify-content-center align-items-center">
+      <canvas
+        ref="canvas"
+        class="bg-dark"
+        :style="{ width: `${GRID_SIZE}px`, height: `${GRID_SIZE}px` }"
+      ></canvas>
+
+      <h1 v-if="message" class="display-6 mt-2">{{ message }}</h1>
+    </section>
   </div>
 </template>
 
---
+++ src/components/VirtualKeyboard.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+const emit = defineEmits(['press']);
+
+</script>
+
+<template>
+  <div class="d-flex justify-content-center align-items-center">
+    <button
+      type="button"
+      class="btn btn-primary btn-lg m-1"
+      @click="emit('press', 'ArrowLeft')"
+    >&larr;</button>
+
+    <div class="d-flex flex-column">
+      <button
+        type="button"
+        class="btn btn-primary btn-lg m-1"
+        @click="emit('press', 'ArrowUp')"
+      >&uarr;</button>
+      <button
+        type="button"
+        class="btn btn-primary btn-lg m-1"
+        @click.down="emit('press', 'ArrowDown')"
+      >&darr;</button>
+    </div>
+
+    <button
+      type="button"
+      class="btn btn-primary btn-lg m-1"
+      @click="emit('press', 'ArrowRight')"
+    >&rarr;</button>
+  </div>
+</template>
+
